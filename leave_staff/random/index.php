ae63cf0f4826d4a22c74d66bb29862964e109ba8
ignratnan <122090143+ignratnan@users.noreply.github.com>
2023-09-20 12:32:23 +0800

    Add files via upload

---
+++ leave_staff/random/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Basic Page Info -->
+	<meta charset="utf-8">
+	<title>Leave System</title>
+
+</head>
+
+<?php include('../includes/config.php'); ?>
+<?php include('../includes/session.php')?>
+
+<body style="background-color: #DCD7C9;">	
+
+
+</body>
+</html>
